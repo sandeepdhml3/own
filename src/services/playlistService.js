cf6d48e5f1065c453355a744279bf1ff3f478157
sandeepdhml3 <133658133+sandeepdhml3@users.noreply.github.com>
2024-07-04 22:37:58 +0545

    Update playlistService.js

--- src/services/playlistService.js
+++ src/services/playlistService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const playlistUrl = 'https://amitb3669.github.io/tataplay6.m3u';
 
-const fetchPlaylist = async () => {
+export const fetchPlaylist = async () => {
   try {
     const response = await axios.get(playlistUrl);
     return parsePlaylist(response.data);
@@ -21,22 +21,12 @@ const parsePlaylist = (data) => {
     if (line.startsWith('#EXTINF')) {
       const channelInfo = line.match(/#EXTINF:-1 tvg-id="(.+?)" group-title="(.+?)", tvg-logo="(.+?)",(.+)/);
       if (channelInfo) {
-        const groupTitle = channelInfo[2];
-        if (groupTitle.includes('SPORTS') || groupTitle.includes('DOCUMENTARIES') || groupTitle.includes('KIDS')) {
-          currentChannel = {
-            id: channelInfo[1],
-            logo: channelInfo[3],
-            group: groupTitle,
-            title: channelInfo[4],
-          };
-        } else if (groupTitle === 'Entertainment' || groupTitle === 'News' || groupTitle === 'HD') {
-          currentChannel = {
-            id: channelInfo[1],
-            logo: channelInfo[3],
-            group: groupTitle,
-            title: channelInfo[4],
-          };
-        }
+        currentChannel = {
+          id: channelInfo[1],
+          logo: channelInfo[3],
+          group: channelInfo[2],
+          title: channelInfo[4],
+        };
       }
     } else if (line.startsWith('http') && currentChannel.id) {
       currentChannel.url = line;
@@ -47,7 +37,3 @@ const parsePlaylist = (data) => {
 
   return channels;
 };
-
-fetchPlaylist().then(channels => {
-  console.log(channels);
-});
