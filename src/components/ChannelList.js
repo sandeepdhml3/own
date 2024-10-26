096e1148afd847871d0b064a7553eaeb7028f6bc
sandeepdhml3 <133658133+sandeepdhml3@users.noreply.github.com>
2024-10-26 18:02:31 +0545

    Update ChannelList.js

--- src/components/ChannelList.js
+++ src/components/ChannelList.js
@@ -5,10 +5,6 @@ const ChannelList = ({ channels, onSelect }) => {
     <div className="channel-list">
       {channels.map(channel => (
         <div
-          key={channel.id}
-          className="channel-item"
-          onClick={() => onSelect(channel)}
-        >
           <img src={channel.logo} alt={channel.title} />
           <div className="channel-title">{channel.title}</div>
         </div>
