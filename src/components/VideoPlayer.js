8c4c0b6363f338c34827c98cfb20f17f298bad99
sandeepdhml3 <133658133+sandeepdhml3@users.noreply.github.com>
2024-10-29 22:38:17 +0545

    Update VideoPlayer.js

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -3,17 +3,25 @@ import ReactPlayer from 'react-player';
 
 import './VideoPlayer.css'; // Import the CSS file
 
-
 const VideoPlayer = ({ src, poster, width = '100%', height = 'auto', channelName }) => {
   return (
     <div className="video-player-wrapper">
       <ReactPlayer
         url={src}
         playing={true} // Auto-play on load
-        controls={true}
+        controls={false} // Disable player controls
         width={width}
         height={height}
         light={poster}
+        playsinline // Ensures the player plays inline on mobile
+        config={{
+          file: {
+            attributes: {
+              controlsList: 'nodownload', // Disable download button
+              disablePictureInPicture: true, // Disable picture-in-picture
+            },
+          },
+        }}
       />
       {channelName && (
         <div className="video-overlay">
