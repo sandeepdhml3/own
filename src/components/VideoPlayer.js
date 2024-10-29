0f9c1773f0a7a1c781a4a680bcaa313e22246f52
sandeepdhml3 <133658133+sandeepdhml3@users.noreply.github.com>
2024-10-29 22:14:05 +0545

    Update VideoPlayer.js

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
 
-const VideoPlayer = ({ src, poster, width = '100%', height = 'auto' }) => {
+const VideoPlayer = ({ src, poster, width = '100%', height = 'auto', channelName }) => {
   return (
     <div className="video-player-wrapper">
       <ReactPlayer
         url={src}
-        playing={false}
+        playing={true} // Auto-play on load
         controls={true}
         width={width}
         height={height}
         light={poster}
       />
+      {channelName && (
+        <div className="video-overlay">
+          <span className="video-channel-name">{channelName}</span>
+        </div>
+      )}
     </div>
   );
 };
